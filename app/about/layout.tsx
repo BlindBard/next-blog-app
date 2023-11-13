33e83fbd4effb0cf6a67d29becdc63ad441da40f
Nick Yanush <m.yanush@apiko.com>
2023-11-13 18:49:03 +0200

    Init

---
+++ app/about/layout.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from "react";
+import Link from "next/link";
+
+export default function AboutLayout({children}:{children: ReactNode}) {
+  return (
+    <div>
+      <h1>About us</h1>
+      <ul>
+        <li>
+          <Link href="/about/contacts">Contacts</Link>
+        </li>
+        <li>
+          <Link href="/about/team">Team</Link>
+        </li>
+      </ul>
+      {children}
+    </div>
+  )
+}
---
+++ components/Footer.tsx
@@ -0,0 +1,8 @@
+const Footer = () => {
+  return(
+    <footer className='container'>
+      Created by &copy;Nick Yanush
+    </footer>
+  )
+}
+export default Footer
